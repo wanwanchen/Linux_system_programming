ed53b52289562fff7fe86757cbd40b7576fbb796
wanwanchen <33883895+wanwanchen@users.noreply.github.com>
2025-10-16 11:47:09 -0600

    simple read/write

--- pipe.c
+++ pipe.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int mian()
+int main()
 {
+	//0 read, 1 write
+	int a[2];
+
+	char buff[64];
+
+	if(pipe(a) == -1)
+	{
+		perror("pipe");
+		exit(1);
+	}
+
+	//write
+	write(a[1], "code", 5);
+	printf("\n");
+
+
+	//read
+	read(a[0], buff, 5);
+	printf("%s\n", buff);
+
+	//write
+	write(a[1], "style", 5);
+	printf("\n");
+
+	//read
+	read(a[0], buff, 5);
+	printf("%s\n", buff);
+
+
 	return 0;
 }
